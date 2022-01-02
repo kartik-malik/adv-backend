7786fd7634c7d83e555ce62d6569c9b5cfd805d5
kartik-malik <kartikmalikboss@gmail.com>
2022-01-02 21:34:41 +0530

    heroku deploying

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("node1", "sammy", "password", {
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  `${process.env.database}`,
+  `${process.env.username}`,
+  `${process.env.password}`,
+  {
+    dialect: "mysql",
+    host: `${process.env.host}`,
+    port: 3306,
+  }
+);
 module.exports = sequelize;
